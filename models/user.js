ebc3847ce547a711ef4513083929641466cedf7d
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-09-29 01:13:13 +1000

    deleted extra comma

--- models/user.js
+++ models/user.js
@@ -1,47 +1,47 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true,
-        }
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
-    {
-        email: {
-            type: String,,
-            required: true,
-            unique: true,
-            match: [/.+@.+/, "Please enter a valid email."]
-        },
-        thoughts: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Thoughts'
-            },
-        ],
-        friends: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "Users"
-            },
-        ],
+  },
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+/, "Please enter a valid email."],
     },
-    {
-        toJSON: {
-            virtuals: true,
-        },
-        
-        id: false,
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thoughts",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Users",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
     },
+
+    id: false,
+  }
 );
 
-userSchema.virtual('friendCount').get(function () {
-    return this.friends.length
-})
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
-const Users = model("Users", userSchema)
+const Users = model("Users", userSchema);
 
-module.exports = Users
+module.exports = Users;
